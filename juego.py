26f1a58f67852f4067beddfa99bb37b41302f28e
Sebastian Loucim <sebastian.loucim@gmail.com>
2022-10-02 20:35:28 -0300

    Tenemos un 10!!! (Initial resources)

--- juego.py
+++ juego.py
@@ -1,6 +1,8 @@
 ORDEN_ESPECIAL = True
+from logging.config import valid_ident
 import random as ran
 import math
+import resource
 from typing import List
 from clases import Asentamiento, Camino, Jugador
 from tablero import TableroCatan
@@ -36,10 +38,22 @@ def tirar_dados():
     dado2 = fastRandomInteger(0, 6) + 1
     return dado1 + dado2
 
-def rellenar_tablero():
+def rellenar_tablero(tablero : TableroCatan):
         # Create a list with all the numbers
-    numberOrder = [5,3,4,4,10,3,9,12,2,9,11,5,11,10]
-    sixEightOrder = [6, 6 ,8 ,8]
+    numberOrder = [2, 3, 3, 4, 4, 5, 5, 9, 9, 10, 10, 11, 11, 12]
+    ran.shuffle(numberOrder)
+
+    sixEightOrder = [6, 6, 8, 8]
+    ran.shuffle(sixEightOrder)
+
+    resourceOrder = [
+        "Piedra", "Piedra", "Piedra",
+        "Ladrillo", "Ladrillo", "Ladrillo",
+        "Trigo", "Trigo", "Trigo", "Trigo",
+        "Madera", "Madera", "Madera", "Madera",
+        "Lana", "Lana", "Lana", "Lana"
+    ]
+    ran.shuffle(resourceOrder)
 
     hexagonArray = [
         [None, None, None, None, None, None, None],
@@ -63,34 +77,52 @@ def rellenar_tablero():
         19: [5, 4],
     }
 
-    placeEverySixOrEight(hexagonArray, spaceCoordinates, sixEightOrder)
+    placeEverySixOrEight(hexagonArray, tablero, spaceCoordinates, sixEightOrder, resourceOrder)
 
     for hexa in range(1, 20):
         if hexagonArray[spaceCoordinates[hexa][0]][spaceCoordinates[hexa][1]] != 0 or hexa == 10: continue
         num = numberOrder.pop()
+        tablero.colocar_recurso_y_numero(hexa, resourceOrder.pop(), num)
         hexagonArray[spaceCoordinates[hexa][0]][spaceCoordinates[hexa][1]] = num
     pass
 
 
 def hasAdjecentSixOrEight(hexagonArray, currentHexagonY, currentHexagonX):
-    for adjacentY in range(currentHexagonY - 1, currentHexagonY + 1):
-        for adjacentX in range(currentHexagonX - 1, currentHexagonX + 1):
+    startX = 0
+    endX = 0
+    for adjacentY in range(currentHexagonY - 1, currentHexagonY + 2):
+
+        if adjacentY != currentHexagonY:
+            if currentHexagonY % 2 == 0:
+                startX = currentHexagonX
+                endX = currentHexagonX + 1
+            else:
+                startX = currentHexagonX - 1
+                endX = currentHexagonX
+        else:
+            startX = currentHexagonX - 1
+            endX = currentHexagonX + 1
+
+        for adjacentX in range(startX, endX + 1):
             if adjacentX == currentHexagonX and adjacentY == currentHexagonY: continue
 
             if hexagonArray[adjacentY][adjacentX] in [6, 8]:
                 return True
     return False
 
-def placeEverySixOrEight(hexagonArray, hexCoordinates, sixEightOrder):
+def placeEverySixOrEight(hexagonArray, tablero, hexCoordinates, sixEightOrder, resourceOrder):
 
     while sixEightOrder:
 
         nextHexagonToPlace = fastRandomInteger(0, 19) + 1
-        if hexagonArray[hexCoordinates[nextHexagonToPlace][0]][hexCoordinates[nextHexagonToPlace][1]] != 0: continue
+        if hexagonArray[hexCoordinates[nextHexagonToPlace][0]][hexCoordinates[nextHexagonToPlace][1]] != 0 or nextHexagonToPlace == 10: continue
+        for hexagonLine in hexagonArray:
+            print(hexagonLine)
         
         if not hasAdjecentSixOrEight(hexagonArray, hexCoordinates[nextHexagonToPlace][0], hexCoordinates[nextHexagonToPlace][1]):
-
-            hexagonArray[hexCoordinates[nextHexagonToPlace][0]][hexCoordinates[nextHexagonToPlace][1]] = sixEightOrder.pop()
+            num = sixEightOrder.pop()
+            tablero.colocar_recurso_y_numero(nextHexagonToPlace, resourceOrder.pop(), num)
+            hexagonArray[hexCoordinates[nextHexagonToPlace][0]][hexCoordinates[nextHexagonToPlace][1]] = num
 
 def jugar_catan(jugadores,tablero):
     # Inicio del juego
@@ -178,7 +210,8 @@ def jugar_turno(jugador, tablero):
 
     return 'continue'
 
-def inicio_juego(jugadores, tablero):
+def inicio_juego(jugadores, tablero: TableroCatan):
+
     for jugador in jugadores:
         print("\n\nPrimer turno inicial de " + jugador.nombre)
         print('v-----------------------------v')
@@ -187,9 +220,11 @@ def inicio_juego(jugadores, tablero):
         camino = Camino(jugador)
 
         asentamientoPos = list(map(int, input("Colocar asentamiento: ").split()))
-        caminoPos = list(map(int, input("Colocar camino: ").split()))
-
         tablero.colocar_asentamiento(asentamientoPos[0], asentamientoPos[1], asentamiento)
+
+        giveInitialResources(tablero, asentamientoPos, asentamiento)
+
+        caminoPos = list(map(int, input("Colocar camino: ").split()))
         tablero.colocar_camino(caminoPos[0], caminoPos[1], camino)
         print('^----------------------------^')
         print("Fin de turno")
@@ -203,23 +238,72 @@ def inicio_juego(jugadores, tablero):
         camino = Camino(jugador)
 
         asentamientoPos = list(map(int, input("Colocar asentamiento: ").split()))
-        caminoPos = list(map(int, input("Colocar camino: ").split()))
-
         tablero.colocar_asentamiento(asentamientoPos[0], asentamientoPos[1], asentamiento)
+
+        giveInitialResources(tablero, asentamientoPos, asentamiento)
+
+
+        caminoPos = list(map(int, input("Colocar camino: ").split()))
         tablero.colocar_camino(caminoPos[0], caminoPos[1], camino)
         print('^----------------------------^')
         print("Fin de turno")
+
     return
 
-Buti = Jugador("Buti", [200, 10, 140])
-Seba = Jugador("Seba", [255, 0, 0])
+def giveInitialResources(tablero: TableroCatan, asentamientoPos, asentamiento):
+    
+    spaceCoordinates = {
+        1: [1, 2],  2: [1, 3],
+        3: [1, 4],  4: [2, 1],
+        5: [2, 2],  6: [2, 3],
+        7: [2, 4],  8: [3, 1],
+        9: [3, 2],  10: [3, 3],
+        11: [3, 4], 12: [3, 5],
+        13: [4, 1], 14: [4, 2],
+        15: [4, 3], 16: [4, 4],
+        17: [5, 2], 18: [5, 3],
+        19: [5, 4],
+    }
 
-#Lista de jugadores
-jugadores = [Seba, Buti]
-#Tablero
-tablero_a_jugar = TableroCatan()
+    fichaAse = asentamientoPos[0]
+    asentamiento.dar_recursos(tablero.obtener_recurso_de_ficha(fichaAse))
+    aristaAse = asentamientoPos[1]
 
-rellenar_tablero(tablero_a_jugar)
+    adjacentSpacesCoordinates = getAdjacentCoordinatesTo(spaceCoordinates[fichaAse])
+    spaces = []
+
+    #Posicion 1 de la posicion adjacente a la arista
+    spaces.append([adjacentSpacesCoordinates[aristaAse - 1][0], adjacentSpacesCoordinates[aristaAse - 1][1]])
+
+    #Posicion 2 de la posicion adjacente a la arista
+    if aristaAse != 6: 
+        spaces.append([adjacentSpacesCoordinates[aristaAse][0], adjacentSpacesCoordinates[aristaAse][1]])
+    else:
+        spaces.append([adjacentSpacesCoordinates[0][0], adjacentSpacesCoordinates[0][1]])
+
+    keyList = list(spaceCoordinates.keys())
+    valList = list(spaceCoordinates.values())
+    for fichaPos in spaces:
+        if fichaPos in valList:
+            ficha = keyList[valList.index(fichaPos)]
+            asentamiento.dar_recursos(tablero.obtener_recurso_de_ficha(ficha))
+
+    return
+
+def getAdjacentCoordinatesTo(space):
+    additive = 0
+    if space[0] % 2 == 0:
+        additive = 1
+
+    return [
+        [space[0] - 1, space[1] -  1 + additive],
+        [space[0] - 1, space[1] + additive],
+        [space[0] + 0, space[1] +  1],
+        [space[0] + 1, space[1] + additive],
+        [space[0] + 1, space[1] -  1 + additive],
+        [space[0] + 0, space[1] -  1]
+    ]
 
 
 
+    
